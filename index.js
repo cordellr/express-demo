eed83f3b99683815f89e0540943b3f34622f06ad
cordellr <cordell.rainock@gmail.com>
2019-06-08 15:56:24 -0500

    experimented with built-in and third party middleware

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const helmet = require("helmet");
+const morgan = require("morgan");
 const Joi = require("joi");
 const logger = require("./logger");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(helmet());
+app.use(morgan("tiny"));
 
 app.use(logger);
 
