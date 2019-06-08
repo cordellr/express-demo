35785e2cec061071c26d4d4cd8a812e1ade80431
cordellr <cordell.rainock@gmail.com>
2019-06-08 16:55:39 -0500

    Storing configuration settings in application and overriding

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require("config");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const Joi = require("joi");
@@ -9,6 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(helmet());
+
+//Configuration
+console.log("Application name: " + config.get("name"));
+console.log("Mail server: " + config.get("mail.host"));
+console.log("Mail password: " + config.get("mail.password"));
 
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
