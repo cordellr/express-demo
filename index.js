54f1919275a74bb528fe40e3557fec429fa2d662
cordellr <cordell.rainock@gmail.com>
2019-06-08 17:36:01 -0500

    Implemented debug statement instead of using console.logs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const debug = require("debug")("app:startup");
 const config = require("config");
 const helmet = require("helmet");
 const morgan = require("morgan");
@@ -18,7 +19,7 @@ console.log("Mail password: " + config.get("mail.password"));
 
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
-  console.log("Morgan enabled...");
+  debug("Morgan enabled...");
 }
 
 app.use(logger);
